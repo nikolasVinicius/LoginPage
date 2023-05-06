406f3484f44221f84c903c354b0fdf823bf7c27c
Nikolas Vinicius <redgamemine2.0@gmail.com>
2023-05-06 03:36:16 -0300

    commit final

--- script/script.js
+++ script/script.js
@@ -2,10 +2,13 @@ function logar () {
     const name = document.getElementById('NickName').value
     const pass = document.getElementById('password').value
 
-    if(name === "melinor" && pass === "manunor02" || name === "manu" && pass === "melinu02"){
+    if(name === "admin" && pass === "admin" || name === "user" && pass === "user"){
         alert(`nice ${name}, bem vindo de volta monstro(a)`);
         location.href = "home/home.html"
-    } else{
+    } else if(name === "user"){
+        location.href = "home/home2.html"
+    }
+     else{
         alert(`${name}??Quem seria voce?... `)
     }
 }
